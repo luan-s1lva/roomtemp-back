e8f7b9dd0f680fc233c035d885184f50102f9220
Luan <l.carmmo141@gmail.com>
2025-02-08 21:17:56 -0300

    cadastro e remoção de salas pronto

--- routes.py
+++ routes.py
@@ -3,8 +3,19 @@ from fastapi.encoders import jsonable_encoder
 from typing import List
 from models import Room
 from database import app
+from fastapi.middleware.cors import CORSMiddleware
 
 router = APIRouter()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @router.get("/show",response_model=List[Room])
 def showTemps(request:Request):
@@ -16,6 +27,6 @@ def addRoom(request:Request, room:Room = Body(...)):
     room = jsonable_encoder(room)
     app.database["salas"].insert_one(room)
 
-@router.delete("/remove/room")
+@router.delete("/remove/room/")
 def removeRoom(request:Request, _id:int):
     app.database["salas"].delete_one({"_id":_id})

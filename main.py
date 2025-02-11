6062736911d403ba911f005eb5517a6576856517
Luan <l.carmmo141@gmail.com>
2025-02-11 19:37:28 -0300

    adicionando lifespan

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import dotenv_values
 from pymongo import MongoClient
 from updateRoomInfo import router as sensor_router
 from routes import router as showAll
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
@@ -10,10 +11,22 @@ import os
 ATLAS_URI = os.getenv("ATLAS_URI")
 DB_NAME = os.getenv("DB_NAME")
 
-
-app = FastAPI()
-
 origins = ["*"]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Iniciando conexão com o MongoDB...")
+    app.mongodb_client = MongoClient("ATLAS_URI")
+    app.database = app.mongodb_client["DB_NAME"]
+    print("Conectado ao MongoDB!")
+
+    yield  # Aqui o app fica rodando
+
+    print("Fechando conexão com o MongoDB...")
+    app.mongodb_client.close()
+    print("Conexão encerrada!")
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,17 +35,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient("ATLAS_URI")
-    app.database = app.mongodb_client["DB_NAME"]
-    print("Connected to the MongoDB database!")
-
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
-
 
 app.include_router(sensor_router, prefix="/temp")
 app.include_router(showAll, prefix="/temp")
